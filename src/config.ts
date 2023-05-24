6a7fa139afa80431c8ed7e0df096ef1aea310733
Dylan Brisson <dylan.brisson@epitech.eu>
2023-05-24 12:13:43 +0200

    portfolio

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Dylan Brisson - Portfolio';
+export const SITE_DESCRIPTION = 'Mon portfolio professionnel Développeur Web JavaScript VueJS Junior codé en AstroJS.';
